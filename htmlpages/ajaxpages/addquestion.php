27d11c5d14bc2650968f644ef14d1132c1533c12
Oddbjørn Haaland <oddbjorn.haaland@bouvet.no>
2012-04-12 12:27:58 +0200

    Improved server validation when adding questions + format

--- htmlpages/ajaxpages/addquestion.php
+++ htmlpages/ajaxpages/addquestion.php
@@ -8,29 +8,56 @@
 // answerX 			: string : the answers
 include_once 'ajaxheader.php';
 require_once ('../quizadmin.php');
-$quizadmin = new quizAdmin ();
-if (!isset ($_POST['quizid'])) {
+$quizadmin = new quizAdmin();
+if (!isset( $_POST['quizid'] )) {
 	//not a proper post. Ignore and die
 	die();
 }
-$questionnumber = $_POST['questionnumber'];
-//might be NULL if new question
-$questiontext = $_POST['questiontext'];
 $quizid = $_POST['quizid'];
-// Can only add question if quiz state is inactive
-if ($quizadmin->getQuizState($quizid) != '0') {
+$questionnumber = $_POST['questionnumber']; //might be NULL if new question
+$questiontext = $_POST['questiontext'];
+
+// Question text must be between 1 and 200 chars
+if (strlen( $questiontext ) > 200 || strlen( $questiontext ) < 1) {
 	die();
 }
-if (isset ($_POST['correctanswer'])) {
+
+// Can only add question if quiz state is inactive
+if ($quizadmin->getQuizState( $quizid ) != '0') {
+	die();
+}
+
+if (isset( $_POST['correctanswer'] )) {
 	$correctanswer = $_POST['correctanswer'];
-} else {
+}
+else {
 	$correctanswer = NULL;
 }
+
+// First answer must be populated 
+if (!isset( $_POST['answer1'] ) || strlen( $_POST['answer1'] ) < 1) {
+	die();
+}
+  
 $answers = array();
-$i=1;
-while(isset($_POST['answer'.$i])) {
-	$answers[$i] = $_POST['answer'.$i];
+$i = 1;
+while (isset( $_POST['answer' . $i] )) {
+	$answers[$i] = $_POST['answer' . $i];
+	// No answer can be longer than 200 characters
+	if (strlen( $answers[$i] ) > 200) {
+		die();
+	}
+	// If answer is marked as correct, it needs to have some content
+	if ($i == $correctanswer && strlen( $answers[$i] ) < 1 ) {
+		die();
+	}
 	$i++;
 }
-$quizadmin->addOrEditQuestion($quizid, $questionnumber, $questiontext, $correctanswer, $answers);
+
+// Correct answer can not be higher than the amount of answers provided
+if ($correctanswer > ($i - 1)) {
+	die();
+}
+
+$quizadmin->addOrEditQuestion( $quizid, $questionnumber, $questiontext, $correctanswer, $answers );
 ?>
