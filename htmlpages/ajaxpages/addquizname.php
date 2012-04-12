92c2e4cc564bda97172c3c549ce97dde9162c89d
Oddbjørn Haaland <oddbjorn.haaland@bouvet.no>
2012-04-12 09:56:27 +0200

    Improved server side validation of adding quiz (made it stricter)
    Now only allows 1-20 (db restriction) nordic alphanumeric characters in keyword
    Now only allows 1-45 (db restriction) characters in quiz name

--- htmlpages/ajaxpages/addquizname.php
+++ htmlpages/ajaxpages/addquizname.php
@@ -10,14 +10,15 @@ if (isset ( $_GET ['quizname'] ) && isset( $_GET ['quizkeyword'] ) ) {
 	$quizName = $_GET ['quizname'];
 	$quizKeyword = strtolower($_GET ['quizkeyword']);
 	
-	// Keyword can not contain spaces
-	$containsSpace = preg_match('/\s/', $quizKeyword);
-	if ($containsSpace) {
-		echo 'Keyword can not contain spaces';
+	// Keyword must be alphanumeric (a-z, 0-9 or æ, Æ, ø, Ø, å, Å, case insensitive) and between 1 and 20 characters
+	$isNordicAlnum = preg_match('/^[a-z0-9\x{00C6}\x{00E6}\x{00C5}\x{00E5}\x{00D8}\x{00F8}]{1,20}$/iu', $quizKeyword);
+	if (!$isNordicAlnum) {
+		echo 'Keyword must be alphanumeric and no longer than 20 characters.';
 		die();
 	}
 
-	if (strlen ( $quizName ) > 0 && strlen( $quizKeyword ) > 0) {
+	// Quiz name length must be between 1 and 200 (DB restriction)
+	if (strlen ( $quizName ) >= 1 && strlen( $quizName ) <= 45) {
 		$quizadmin->addQuizName ( $quizName, $quizKeyword );
 	}
 }
