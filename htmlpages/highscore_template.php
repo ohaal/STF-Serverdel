38839cecf360f7fbe586e6f6adb6357774454aba
Oddbjørn Haaland <oddbjorn.haaland@bouvet.no>
2012-04-26 14:21:22 +0200

    Switched doctype to HTML5

--- htmlpages/highscore_template.php
+++ htmlpages/highscore_template.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html>
 <html>
   <head>
     <link type="text/css" href="css/custom-theme/jquery-ui-1.8.17.custom.css" rel="stylesheet" />
