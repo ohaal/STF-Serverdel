97f0d4a2f93edd1d9e7ca436d0d6ad1f763c6755
Oddbjørn Haaland <oddbjorn.haaland@bouvet.no>
2012-05-31 12:54:06 +0200

    Changed how MMS are handled after receiving - now allows several pictures and texts in same message

--- htmlpages/mmsreceive.php
+++ htmlpages/mmsreceive.php
@@ -31,22 +31,26 @@ class MMSReceiveHandler {
 		}
 		$savepath = $savepath.SEP;
 		$zipfilepath = $savepath.'data.zip';
-		file_put_contents($zipfilepath, $data);
+		// Put zip data in file
+		if (file_put_contents($zipfilepath, $data) === false) {
+			error_log('Unable to save MMS data to file', 0);
+			return false;
+		}
 	
 		// Extract files in same folder as zipfile
 		if (!$this->unzip($zipfilepath, $savepath, true, true)) {
-			error_log('Unable to unzip '.$zipfilepath, 0);
+			error_log('Unable to unzip file, path: '.$zipfilepath, 0);
 			return false;
 		}
 		// Remove zip file
-		if (!unlink($zipfilepath)) {
-			error_log('Unable to delete file '.$zipfilepath, 0);
-		}
+//		if (!unlink($zipfilepath)) {
+//			error_log('Unable to delete file '.$zipfilepath, 0);
+//		}
 	
 		// Get name of image and get name of files containing text message from smil.xml
 		$xmlfilepath = $savepath.'smil.xml';
 		if (!file_exists($xmlfilepath)) {
-			error_log('Could not find XML with data '.$xmlfilepath, 0);
+			error_log('Could not find XML with data, path: '.$xmlfilepath, 0);
 			return false;
 		}
 		$xmlobj = simplexml_load_file($xmlfilepath);
@@ -55,90 +59,113 @@ class MMSReceiveHandler {
 //			error_log('Unable to delete file '.$xmlfilepath, 0);
 //		}
 		
-		// Get image filename
-		$imgfile = (string)$xmlobj->body->par->img->attributes()->src;
-		if (empty($imgfile)) {
-			error_log('Could not get image file name from XML', 0);
-			return false;
-		}
-		$imgfilepath = $savepath.$imgfile;
-		if (!file_exists($imgfilepath)) {
-			error_log('Could not find MMS image file '.$imgfilepath, 0);
-			return false;
-		}
-		// Move file to parent directory and rename to something more sensible (its timestamp)
-		$newpath = $savepath.'..'.SEP.$timestamp.'.jpg';
-		$renamesuccess = rename($imgfilepath, $newpath);
-		if ($renamesuccess) {
-			$imgfilepath = realpath($newpath);
-		}
-		else {
-			// Fallback solution: look for the file in the specific folder
-			$files = glob($savepath.'*.jpg');
-			if (count($files) != 1) {
-				error_log('Warning: Unexpectedly more than one .jpg file in '.$savepath.', using first file', 0);
-			}
-			// Try renaming again
-			$renamesuccess = rename($files[0], $newpath);
-			if (!$renamesuccess) {
-				error_log('Could not rename MMS image file '.$files[0], 0);
-				$imgfilepath = realpath($newpath);
+		// Grab each message blob and related media
+		$i = 1;
+		$dataobjects = $xmlobj->body->par;
+		foreach ($dataobjects as $dataobj) {
+			// Get image filename
+			$imgfile = (string)$dataobj->img->attributes()->src;
+			if (empty($imgfile)) {
+				error_log('Could not get image file name from XML', 0);
 				return false;
 			}
-		}
-		
-		// Check for text message file(s) in XML 
-		$texts = $xmlobj->body->par->text;
-		$first = true;
-		foreach ($texts as $text) {
-			$txtfile = (string)$text->attributes()->src;
-			if (!empty($txtfile)) {
-				$txtfilepath = $savepath.$txtfile;
-				if (file_exists($txtfilepath)) {
-					// Get message from text file
-					$msgpart = file_get_contents($txtfilepath);
-					// ?: First part of message
-					if ($first) {
-						// -> Remove default keyword from start of sentence
-						$e = explode(' ', $msgpart, 2);
-						if (count($e) > 1 && strtolower($this->config['keywords_default']) == strtolower($e[0])) {
-							$msgpart = $e[1];
-						}
- 						// -> Add space between subject and actual message if user sent text in subject
- 						if (!empty($message)) {
-							$msgpart = ' '.$msgpart;
- 						}
-						$first = false;
+			$imgfilepath = $savepath.$imgfile;
+			if (!file_exists($imgfilepath)) {
+				// For some reason, certain MMS file names are listed differently in XML than in filesystem, this is a workaround
+				// Fallback solution: look for the file in the specific folder
+				$files = glob($savepath.'*.jpg');
+				$match = false;
+				// See if any of the files match the current expected name
+				foreach ($files as $filename) {
+					$basename = basename($filename);
+					// Match basename case-insensitive at end of line of expected name
+					//                            .-------´
+					if (preg_match('/'.$basename.'$/i', $imgfile) || count($files) == 1) {
+						$imgfilepath = $filename;
+						$match = true;
+						break;
 					}
-					
-					// Append message part to message
-					$message .= $msgpart;					
-					// Remove text file
-//					if (!unlink($txtfilepath)) {
-//						error_log('Unable to delete file '.$txtfilepath, 0);
-//					}
 				}
-				else {
-					error_log('Could not find text message file '.$txtfilepath, 0);
+				if (!$match) {
+					error_log('Could not match any files up against imgfile '.$imgfile, 0);
+					return false;
+				}
+				// If it still doesn't exist, we're out of options
+				if (!file_exists($imgfilepath)) {
+					error_log('Could not find MMS image file, path: '.$imgfilepath, 0);
+					return false;
 				}
 			}
+			$x = '';
+			if ($i > 1) {
+				$x = '-'.$i;
+			}
+			// Move file to parent directory and rename to something more sensible (its timestamp)
+			$newpath = $savepath.'..'.SEP.$timestamp.$x.'.jpg';
+			$renamesuccess = rename($imgfilepath, $newpath);
+			if (!$renamesuccess) {
+				error_log('Could not rename MMS image file, path: '.$imgfilepath, 0);
+				return false;
+			}
+			$newpath = realpath($newpath);
+			
+			// Get texts related to message
+			$texts = $dataobj->text;
+			$first = true;
+			$msg = '';
+			foreach ($texts as $text) {
+				$txtfile = (string)$text->attributes()->src;
+				if (!empty($txtfile)) {
+					$txtfilepath = $savepath.$txtfile;
+					if (file_exists($txtfilepath)) {
+						// Get message from text file
+						$msgpart = file_get_contents($txtfilepath);
+						// ?: First part of message
+						if ($first) {
+							// -> Remove default keyword from start of sentence
+							$e = explode(' ', $msgpart, 2);
+							if (count($e) > 1 && strtolower($this->config['keywords_default']) == strtolower($e[0])) {
+								$msgpart = $e[1];
+							}
+							$first = false;
+						}
+						
+						// Append message part to message
+						$msg .= $msgpart;					
+						// Remove text file
+//						if (!unlink($txtfilepath)) {
+//							error_log('Unable to delete file '.$txtfilepath, 0);
+//						}
+					}
+					else {
+						error_log('Warning: Could not find text message file '.$txtfilepath, 0);
+					}
+				}
+			}
+			if (empty($msg)) {
+				$msg = $message;
+			}
+			
+			// Trim double whitespace in message
+			$msg = preg_replace('/(\s){2,}/', '${1}', $msg);
+			// Trim whitespaces at end of message
+			$msg = trim($msg);			
+			
+			// Add MMS information to database and push to MMSadmin
+			$mms = new mmsReaction();
+			if ($mms->addMms($phonenumber, $msg, $imgfilepath) < 0) {
+				error_log('Could not add MMS information to database, path: '.$savepath, 0);
+				return false;
+			}			
+			
+			$i++;
 		}
-		// Remove folder
-		if (!rmdir($savepath)) {
-			error_log('Unable to delete folder '.$savepath, 0);
-		}
-
-		// Trim double whitespace in message
-		$message = preg_replace('/(\s){2,}/', '${1}', $message);
-		// Trim whitespaces at end of message
-		$message = trim($message);
 		
-		// Add MMS information to database and push to MMSadmin
-		$mms = new mmsReaction();
-		if ($mms->addMms($phonenumber, $message, $imgfilepath) < 0) {
-			error_log('Could not add MMS information to database', 0);
-			return false;
-		}
+		// Remove folder
+//		if (!rmdir($savepath)) {
+//			error_log('Unable to delete folder '.$savepath, 0);
+//		}
+
 		return true;
 	}
 	
@@ -151,12 +178,12 @@ class MMSReceiveHandler {
 	 * @param   boolean     Indicates if the files will be unpacked in a directory with the name of the zip-file (true) or not (false) (only if the destination directory is set to false!)
 	 * @param   boolean     Overwrite existing files (true) or not (false)
 	 * 
-	 * @return  boolean     Succesful or not
+	 * @return  boolean     Successful or not
 	 */
 	private function unzip($src_file, $dest_dir = false, $create_zip_name_dir = true, $overwrite = true) {
 		if ($zip = zip_open( $src_file )) {
 			if (is_resource($zip)) {
-				$splitter = ($create_zip_name_dir === true) ? "." : "/";
+				$splitter = ($create_zip_name_dir === true) ? "." : SEP;
 				if ($dest_dir === false) $dest_dir = substr( $src_file, 0, strrpos( $src_file, $splitter ) ) . "/";
 				
 				// Create the directories to the destination dir if they don't already exist
@@ -168,7 +195,7 @@ class MMSReceiveHandler {
 					
 
 					// If the file is not in the root dir
-					$pos_last_slash = strrpos( zip_entry_name( $zip_entry ), "/" );
+					$pos_last_slash = strrpos( zip_entry_name( $zip_entry ), SEP );
 					if ($pos_last_slash !== false) {
 						// Create the directory where the zip-entry should be saved (with a "/" at the end)
 						$this->create_dirs( $dest_dir . substr( zip_entry_name( $zip_entry ), 0, $pos_last_slash + 1 ) );
